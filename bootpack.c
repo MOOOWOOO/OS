1dc76f6f3d2180d1c10840546dd7c78bed79b41e
MOOOWOOO <MOOOWOOO@live.com>
2014-12-08 22:35:55 +0800

    Day 5: 新增在屏幕上打印出 A 字符的函数和操作。

--- bootpack.c
+++ bootpack.c
@@ -9,6 +9,8 @@ void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 void init_screen(char *vram, int x, int y);
 
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+
 #define COL8_000000		0
 #define COL8_FF0000		1
 #define COL8_00FF00		2
@@ -36,10 +38,15 @@ struct BOOTINFO
 void HariMain(void)
 {
 	struct BOOTINFO *binfo;
+	static char font_A[16] = {
+		0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
+		0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00
+	};
 
 	init_palette();	// 设定调色板
 	binfo = (struct BOOTINFO *) 0x0ff0;	// asmhead.nas 中 BOOT_INFO 部分
 	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+	putfont8(binfo->vram, binfo->scrnx, 10, 10, COL8_FFFFFF, font_A);
 
 	while (1) {
 		io_hlt();
@@ -117,4 +124,21 @@ void init_screen(char *vram, int xsize, int ysize) {
 	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize -  3, xsize -  4, ysize -  3);
 	boxfill8(vram, xsize, COL8_FFFFFF, xsize -  3, ysize - 24, xsize -  3, ysize -  3);
 	return ;
+}
+
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font) {
+	int i;
+	char d, *p; // data
+	for (i = 0; i < 16; i++) {
+		p = vram + (y + i) * xsize + x;
+		d = font[i];
+		if ((d & 0x80) != 0) { p[0] = c;}
+		if ((d & 0x40) != 0) { p[1] = c;}
+		if ((d & 0x20) != 0) { p[2] = c;}
+		if ((d & 0x10) != 0) { p[3] = c;}
+		if ((d & 0x08) != 0) { p[4] = c;}
+		if ((d & 0x04) != 0) { p[5] = c;}
+		if ((d & 0x02) != 0) { p[6] = c;}
+		if ((d & 0x01) != 0) { p[7] = c;}
+	}
 }
