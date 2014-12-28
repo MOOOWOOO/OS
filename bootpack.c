0d10e66e7224d5278a1ac2f222a218f2eb4498b7
MOOOWOOO <MOOOWOOO@live.com>
2014-12-28 21:35:16 +0800

    Day 7:
    鼠标中断

--- bootpack.c
+++ bootpack.c
@@ -17,6 +17,8 @@ void HariMain(void)
 	io_out8(PIC0_IMR, 0xf9);
 	io_out8(PIC1_IMR, 0xef);
 
+	init_keyboard();
+
 	init_palette();	// 设定调色板
 	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
 	mx = (binfo->scrnx - 16) / 2; /* 鼠标置于屏幕中部 */
@@ -27,7 +29,10 @@ void HariMain(void)
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-	fifo8_init(&keyfifo, 32, keybuf);
+	// fifo8_init(&keyfifo, 32, keybuf);
+	// 为何上面这句需要被注释？
+
+	enable_mouse();
 
 	while (1) {
 		io_cli();
@@ -41,4 +46,48 @@ void HariMain(void)
 			putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
 		}
 	}
+}
+
+/**
+ * 等待键盘控制电路准备完毕
+ */
+#define PORT_KEYDAT				0x0060
+#define PORT_KEYSTA				0x0064
+#define PORT_KEYCMD				0x0064
+#define KEYSTA_SEND_NOTREADY	0x02
+#define KEYCMD_WRITE_MODE		0x60
+#define KBC_MODE				0x47
+
+void wait_KBC_sendready(void) {
+	while(1) {
+		if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0) {
+			break;
+		}
+	}
+	return ;
+}
+
+/**
+ * 初始化键盘控制电路
+ */
+void init_keyboard(void) {
+	wait_KBC_sendready();
+	io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+	wait_KBC_sendready();
+	io_out8(PORT_KEYDAT, KBC_MODE);
+	return ;
+}
+
+/**
+ * 激活鼠标
+ */
+#define KEYCMD_SENDTO_MOUSE		0xd4
+#define MOUSECMD_ENABLE			0xf4
+
+void enable_mouse(void) {
+	wait_KBC_sendready();
+	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+	wait_KBC_sendready();
+	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+	return ;		// 如果顺利，键盘控制器会返回ACK(0xfa)
 }
